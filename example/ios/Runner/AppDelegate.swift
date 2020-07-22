febb72eab9c295bae1ed1a9776e4cb1d55fcdb9e
sam-parks <parkssa123@gmail.com>
2020-07-22 07:36:30 -0400

    handle url schemes

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import torus_direct
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,4 +11,13 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    print("hello")
+      
+    TorusSwiftDirectSDK.handle(url: url)
+      
+    return true
+  }
 }
+
--- ios/Classes/SwiftTorusDirect.swift
+++ ios/Classes/SwiftTorusDirect.swift
@@ -4,6 +4,25 @@ import SafariServices
 
 public class SwiftTorusDirectPlugin: NSObject, FlutterPlugin {
 
+  let torusSwiftDirectSDK: TorusSwiftDirectSDK;
+
+  
+  override public init(){
+
+   let subVerifierDetails =  SubVerifierDetails(loginType: .installed,
+                                                    loginProvider: .google,
+                                                    clientId: "653095671042-san67chucuujmjoo218khq2rb92bh80d.apps.googleusercontent.com",
+                                                    verifierName: "google-test-1594836702346",
+                                                    redirectURL: "com.googleusercontent.apps.653095671042-san67chucuujmjoo218khq2rb92bh80d:/oauthredirect")
+
+    self.torusSwiftDirectSDK = TorusSwiftDirectSDK(aggregateVerifierType: .singleLogin, 
+                                                            aggregateVerifierName: "google-test-1594836702346", 
+                                                            subVerifierDetails: [subVerifierDetails], loglevel: .trace)
+    super.init()                                              
+  }
+
+
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "torus.flutter.dev/torus-direct", binaryMessenger: registrar.messenger())
     let instance = SwiftTorusDirectPlugin()
@@ -13,16 +32,7 @@ public class SwiftTorusDirectPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
         case "triggerLogin":
-        let subVerifierDetails =  SubVerifierDetails(loginType: .installed,
-                                                    loginProvider: .google,
-                                                    clientId: "238941746713-vfap8uumijal4ump28p9jd3lbe6onqt4.apps.googleusercontent.com",
-                                                    verifierName: "google-ios",
-                                                    redirectURL: "com.googleusercontent.apps.238941746713-vfap8uumijal4ump28p9jd3lbe6onqt4:/oauthredirect")
-
-        let torusSwiftDirectSDK = TorusSwiftDirectSDK(aggregateVerifierType: .singleIdVerifier, 
-                                                            aggregateVerifierName: "multigoogle-torus", 
-                                                            subVerifierDetails: [subVerifierDetails])
-            torusSwiftDirectSDK.triggerLogin(browserType: .external).done
+            self.torusSwiftDirectSDK.triggerLogin(browserType: .external).done
             { 
               data in print("private key rebuild", data)
               result(data)
