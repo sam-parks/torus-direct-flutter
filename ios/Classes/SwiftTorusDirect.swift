f1f8ece6b5742a0c5ffdcc4c03bd676a518581ba
sam-parks <parkssa123@gmail.com>
2020-07-23 09:49:09 -0500

    deleted init method

--- ios/Classes/SwiftTorusDirect.swift
+++ ios/Classes/SwiftTorusDirect.swift
@@ -3,25 +3,9 @@ import UIKit
 import SafariServices
 
 public class SwiftTorusDirectPlugin: NSObject, FlutterPlugin {
-  var torusSwiftDirectSDK: TorusSwiftDirectSDK;
-  var subVerifierDetails: SubVerifierDetails;
+    var torusSwiftDirectSDK: TorusSwiftDirectSDK? = nil
+    var subVerifierDetails: SubVerifierDetails? =  nil
   
-  override public init(){
-
-    subVerifierDetails =  SubVerifierDetails(loginType: .installed,
-                                                    loginProvider: .google,
-                                                    clientId: "653095671042-san67chucuujmjoo218khq2rb92bh80d.apps.googleusercontent.com",
-                                                    verifierName: "tokenizer-google-ios",
-                                                    redirectURL: "com.googleusercontent.apps.653095671042-san67chucuujmjoo218khq2rb92bh80d:/oauthredirect")
-
-    self.torusSwiftDirectSDK = TorusSwiftDirectSDK(aggregateVerifierType: .singleLogin, 
-                                                            aggregateVerifierName: "tokenizer-google-ios", 
-                                                            subVerifierDetails: [subVerifierDetails], loglevel: .trace)
-    super.init()                                              
-  }
-
-
-
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "torus.flutter.dev/torus-direct", binaryMessenger: registrar.messenger())
     let instance = SwiftTorusDirectPlugin()
@@ -49,10 +33,10 @@ public class SwiftTorusDirectPlugin: NSObject, FlutterPlugin {
                                                     redirectURL: redirectURL)
             self.torusSwiftDirectSDK = TorusSwiftDirectSDK(aggregateVerifierType: verifierTypes(rawValue: verifierTypeString)!, 
                                                             aggregateVerifierName: verifierName, 
-                                                            subVerifierDetails: [subVerifierDetails], loglevel: .trace)
+                                                            subVerifierDetails: [subVerifierDetails!], loglevel: .trace)
 
         case "triggerLogin":
-            self.torusSwiftDirectSDK.triggerLogin(browserType: .external).done
+            self.torusSwiftDirectSDK!.triggerLogin(browserType: .external).done
             { 
               data in print("private key rebuild", data)
               result(data)
